64c0988bde3817f8f0c0990b2ba7c613c4ac9271
Deividas <dilgunas2003@gmail.com>
2024-11-01 20:10:55 -0400

    Got stats working properly Need to add Save, Add comments, fix up the code

--- Quiz_Game/main.cpp
+++ Quiz_Game/main.cpp
@@ -27,19 +27,5 @@ int main() {
 
     stat.stat(info, score);
 
-/*    std::cout << "Quiz Game Stats: " << std::endl << "=========================" << std::endl;
-
-    std::cout << "Player " << info.name << " is " << info.age << ". Below are the stats for today!"
-              << std::endl << std::endl;
-
-    std::cout << "Gaming: " << score.points.gaming << "/10" << std::endl;
-    std::cout << "Food: " << score.points.food << "/10" << std::endl;
-    std::cout << "Movies/Shows: " << score.points.moviesShows << "/10" << std::endl;
-    std::cout << "Music: " << score.points.music << "/10" << std::endl;
-    std::cout << "Math: " << score.points.math << "/10" << std::endl;
-    std::cout << "Tech: " << score.points.tech << "/10" << std::endl;
-    std::cout << "Sports: " << score.points.sports << "/10" << std::endl;
-    std::cout << "History: " << score.points.history << "/10" << std::endl; */
-
     return 0;
 }
